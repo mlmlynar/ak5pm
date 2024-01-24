4f81ea14d1701c7d6f90b3a0f8d6f4f8792e4b43
Martin Mlynár <m_mlynar@utb.cz>
2024-01-24 11:21:03 +0100

    kurz.page.html -> zmena vzhladu kurz.page.ts -> doimplementovanie ukladania do pamat, preddefinovanie defaultnych hodnot

--- src/app/Navigation/kurz/kurz.page.ts
+++ src/app/Navigation/kurz/kurz.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CurrencyAPIService } from 'src/app/api/currency-api.service';
+import { HistoryServiceService } from 'src/app/service/history.service';
 
 @Component({
   selector: 'app-kurz',
@@ -11,18 +12,36 @@ export class KurzPage implements OnInit {
 
   
   hodnota: number = 1
-  zmeny: String = ''
-  domeny: String = ''
-  currencyOutput$: Observable<any> = this.apiService.getCurrency("","")
+  zmeny: String = 'EUR'
+  domeny: String = 'CZK'
+  currencyOutput$: Observable<any> = this.apiService.getCurrency("","");
+  historyArray: Array<String> = []
   
-  constructor(private apiService: CurrencyAPIService) { }
+  constructor(
+    private apiService: CurrencyAPIService,
+    private storage:HistoryServiceService
+    ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
   }
-
+  async ionViewDidEnter() {
+    const storedData = await this.storage.get('history');
+    if(storedData){ 
+      this.historyArray = JSON.parse(storedData);
+    }
+  }
+ 
   preved(){
     console.log(this.hodnota + ' ' + this.zmeny + ' ' + this.domeny + ' = ' + this.currencyOutput$);
     this.currencyOutput$ = this.apiService.getCurrency(this.zmeny,this.domeny)
+
+    this.currencyOutput$.subscribe(data =>{
+      let poPrevode   = data.conversion_rate * this.hodnota;
+      let historyItem = `${this.hodnota} ${this.zmeny} | ${poPrevode} ${this.domeny} || Kurz : ${data.conversion_rate}`;
+      console.log(historyItem);
+      this.historyArray.unshift(historyItem);
+      this.storage.set('history',JSON.stringify(this.historyArray));
+    });
     
     
   }
